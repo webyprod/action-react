1702e276c668111f2505c998705f5d402fa45ff9
fox <mk.alex1989@gmail.com>
2025-05-04 18:52:22 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+// this is app component
+function App() {
+
+  return (
+    <>
+      <h1>CI/CD using Github Actions</h1>
+      <h2>For beginners v2</h2>
+      <h2>With 2 projects</h2>
+    </>
+  )
+}
+
+export default App
